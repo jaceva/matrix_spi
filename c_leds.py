2765cebc7ba46eec6fdf067cbd26999ada435086
jacev <jaceva@gmail.com>
2022-01-28 08:10:06 -0500

    effect frame chasing update

--- c_leds.py
+++ c_leds.py
@@ -15,17 +15,25 @@ class MatrixLEDs():
     self.spi = None
     self.power = 1
     self.speed = 5
-    self.data = 'blue'
-    self.prev_data = 'blue'
+    self.eff_red = 0
+    self.eff_green = 0
+    self.eff_blue = 0
+    self.effect = 'eff-white-col-right'
+    self.prev_effect = 'blue'
+    # whether data can be rgb controlled
+    self.is_rgb = False
 
     self.get_main_data()
     self.init_spi()
 
   def get_main_data(self):
     data_files = listdir("/home/pi/matrix_spi/data")
+    
     for f in data_files:
-      with open(f"/home/pi/matrix_spi/data/{f}", 'rb') as pk_file:
-        self.main_data[f] = pickle.load(pk_file)
+      
+      if ".npy" not in f:
+        with open(f"/home/pi/matrix_spi/data/{f}", 'rb') as pk_file:
+          self.main_data[f] = pickle.load(pk_file)
 
   def init_spi(self):
     print("SPI Init")
@@ -38,10 +46,14 @@ class MatrixLEDs():
     self.spi.open(bus, device)
     self.spi.max_speed_hz = 4000000
 
-  def set_data(self, power, speed, data):
+  def set_data(self, power=1, speed=5, effect='blue',
+                red=0, green=0, blue=0):
     self.power = power
     self.speed = speed
-    self.data = data
+    self.effect = effect
+    self.eff_red = red
+    self.eff_green = green
+    self.eff_blue = blue
 
   def start_spi(self):
     spi_thread = Thread(name='spi_loop', target=self.run_spi)
@@ -49,57 +61,61 @@ class MatrixLEDs():
 
   def run_spi(self):
     print("SPI Running")
+
+
     
+    frame_total = 0   # frames in effect 
+    frame_count = 0   # counter for frames
 
-    # counter for movie data
-    frame_count = 0
+    refresh_count = 0
 
-    # frame index?
-    data_index = 0
-
-    # TODO start testing
-    # YOU HAVE DISABLED UWSGI SERVICE FOR TESTING
-    test_index = 0
-    test_data1 = get_color_array(r=255, g=255, b=255)
-    # test_data2 = get_vline_array(r1=0, g1=0, b1=255, 
-    #                              r2=255, g2=0, b2=0, )
-    manip_time = 0
-    
     # seconds per frame
     spf = 0.05
-    # TODO for testing
-    # spf = 1
 
-    spi_time = 0
-    white_level = 0
-    step = -5
+    self.speed = 10
     
+    # white_level = 0
+    # step = -5
+    
+    manip_time = 0
+    spi_time = 0
+
     while True:
+      print(time.time() - spi_time)
       if (time.time() - spi_time) > spf:
         spi_time = time.time()
-        with self.lock:
-          manip_time = time.time()
-          # manip_data = bitmanip(test_data1)
-          manip_data = bitmanip(get_color_array(r=0, g=0, b=white_level))
-          
-          # break
-          # print(len(manip_data))
-          # print(time.time() - manip_time)
-          # self.spi.xfer3(manip_data)
-          # print(manip_data)
-          
-          # spi send
-          # self.cs_pin.off()
-          send_time = time.time()
-          self.spi.writebytes2(manip_data)
-          send_time = time.time() - send_time
-          print(send_time)
-          # self.cs_pin.on()
+        if self.effect != self.prev_effect:
+          self.prev_effect = self.effect
+          effect_frames = np.load(f"/home/pi/matrix_spi/data/{self.effect}.npy")
+          frame_total = effect_frames.shape[0]
+          refresh_count = 0
+        if refresh_count >= (10-self.speed):
+          refresh_count = 0
+          with self.lock:
+            # manip_time = time.time()
+            manip_data = bitmanip(effect_frames[frame_count])
+            # print(manip_data.shape)
+            # manip_data = bitmanip(get_color_array(r=0, g=0, b=white_level))
+            # print(time.time() - manip_time)
+            
+            # self.cs_pin.off()
+            send_time = time.time()
+            self.spi.writebytes2(manip_data)
+            send_time = time.time() - send_time
+            # print(send_time)
+            # self.cs_pin.on()
 
-        if white_level == 255 or white_level == 0:
-          step *= -1
+          # next effect frame
+          frame_count += 1
+          if frame_count == frame_total:
+            frame_count = 0
 
-        white_level += step
+        # effect speed count
+        refresh_count += 1
+        # if white_level == 255 or white_level == 0:
+        #   step *= -1
+
+        # white_level += step
 
     # TODO Commented out to simplify loop for error testing 
     # first send (for static images)
--- image_process.py
+++ image_process.py
@@ -2,55 +2,68 @@ import ffmpeg
 import numpy as np
 import pickle
 
-
-# out, _ = ffmpeg.input('sparkle.gif').filter('scale', 108, 36).output('pipe:', format='rawvideo', pix_fmt='rgb24').run(capture_stdout=True)
-# video = np.frombuffer(out, np.uint8).reshape([-1, 36, 108, 3])
-
-# print(video.shape)
+# ffmepeg to covert images(gifs?) to the right size numpy array
+# TODO needs testing
+def convert_image(image):
+  try:
+    out, _ = ffmpeg.input('imports/' + image).filter('scale', 108, 36).output('pipe:', format='rawvideo', pix_fmt='rgb24').run(capture_stdout=True)
+    video = np.frombuffer(out, np.uint8).reshape([-1, 36, 108, 3])
+    
+    print(video[0][14])
+  except Exception as e:
+    print(e)
+  
 
 def get_full_image():
-  return np.zeros((36, 108, 3), dtype=np.ubyte)
+  return np.zeros((1, 36, 108, 3), dtype=np.ubyte)
 
-def get_spi_data(np_array):
-  # TODO the following 2 loops need to be preprocess and stored
-  # each entry of bit data represents one bit for 6 strands of a controller
-  # 24 entries equal the color of a column of leds on the 6 strands of a controller
-  bit_data = [0] * (6*8*108*3)
+def white_up(step):
+  if step > 0:
+    effect = np.zeros((1, 36, 108, 3), dtype=np.ubyte)
+    frame = get_full_image()
+    level = 0
+    while level <= 255:
+      frame[:,:,:] = level
+      level += step
+      effect = np.append(effect, frame, axis=0)
+    
+    effect = effect[1:]
+    return effect
 
-  for con in range(6):
-    for led in range(108):
-      for color in range(3):
-        for strand, color_data in enumerate(np_array[con*6:con*6+6,led, color]):
-          for byte in range(8):
-            index = (con*2592)+(led*24)+(8*color)+byte
-            bit_data[index] |= (((color_data >> byte) & 1) << strand)
-          # print(f"Controller: {con}, Strand: {con*6+strand}, LED: {led}, Color: {color}, Data: {bit_data[index]}")
-  # print([bin(b) for b in bit_data])
+def column_chase():
+  effect = np.zeros((1, 36, 108, 3), dtype=np.ubyte)
+  frame = get_full_image()
 
-  spi_data = []
+  for column in range(108):
+    frame[:, :, :, :] = 0
+    frame[:, :, column, :] = 255
+    effect = np.append(effect, frame, axis=0)
 
-  for i in range(int(len(bit_data)/4)):
-    # Check this data. It looks maybe off in serial output.
-    spi_index = 4*i
-    spi_data.append(int(bit_data[spi_index] | bit_data[spi_index+1] << 6) & 0xff)
-    spi_data.append(int((bit_data[spi_index+1] >> 2) | bit_data[spi_index+2] << 4) & 0xff)
-    spi_data.append(int((bit_data[spi_index+2] >> 4) | bit_data[spi_index+3] << 2) & 0xff)
-
-  return spi_data
-
-def write_spi_to_file(data):
-  with open("./data/white-pulse-slow", "wb") as pk_file:
-    pickle.dump(data, pk_file)
+  effect = effect[1:]
+  return effect
 
 
 
-spi_data = []
-image1 = get_full_image()
-lvl = 0
-step = 8
-while lvl <= 255:
-  print(lvl)
-  image1[:,:,:] = lvl
-  lvl += step
-  spi_data.append(get_spi_data(image1))
-write_spi_to_file(spi_data)
+    
+
+  return None
+  
+
+if __name__ == "__main__":
+  # convert_image('sparkle.gif')
+  # e = white_pulse(10)
+
+
+  e = column_chase()
+  print(e.shape[0])
+  np.save("data/eff-white-col-right", e)
+
+
+
+# image1 = get_full_image()
+# lvl = 0
+# step = 8
+# while lvl <= 255:
+#   print(lvl)
+#   image1[:,:,:] = lvl
+#   lvl += step
